eba53982f1d71af001a8e9bc18826949d90147ba
goopsta <81267230+goopsta@users.noreply.github.com>
2022-03-17 15:20:42 -0700

    Update prem.lua

--- prem.lua
+++ prem.lua
@@ -1,2 +1,2 @@
-getgenv().premTable = {61269716, 1726793709, 1577814518, 300478125, 3202368300}
+getgenv().premTable = {61269716, 1726793709, 1577814518, 300478125, 3202368300, 72096580}
 -- all this uses userid and not username btwwwwww
